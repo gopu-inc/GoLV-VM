1bc78d7484004dea6f185e1dfc173d0a5a5818a7
MauricioKk-ship-it <bilesibelbiche@gmail.com>
2025-12-19 02:06:12 +0100

    Update __init__.py

--- golv/__init__.py
+++ golv/__init__.py
@@ -3,7 +3,7 @@ GoLV Python SDK - Terminal sécurisé pour IA
 """
 
 __version__ = "1.0.0"
-__author__ = "GoLV Team"
+__author__ = "GOPU.inc"
 
 from .client import GoLVClient
 from .agent import GoLVAgent
